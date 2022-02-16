688781afd088bda952c621abe9e4a7b1bab2e31a
kitkatlearner <98730721+kitkatlearner@users.noreply.github.com>
2022-02-16 10:32:10 +0530

    Add files via upload

---
+++ Who will pay the bill.py
@@ -0,0 +1,5 @@
+import random
+names=input('Gimme da names: ')
+names_input=names.split(', ')
+random_names=len(names_input)
+print(names_input[random.randint(0,random_names)], 'will pay the bill.')
