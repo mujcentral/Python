f5375953c13d312fc6ac3f61de60c7546539d5ea
The-Batman-Code <98730721+The-Batman-Code@users.noreply.github.com>
2022-03-03 22:52:07 +0530

    Delete who will pay the bill.py

--- who will pay the bill.py
+++
@@ -1,5 +0,0 @@
-import random
-names=input('Gimme da names: ')
-names_input=names.split(', ')
-random_names=len(names_input)
-print(names_input[random.randint(0,random_names-1)], 'will pay the bill.')
